f9072f9b467a03417001c974f8e5d62a679c6240
Gabriel Wohlford <GabrielWohlford@gmail.com>
2020-06-23 14:34:02 -0600

    Removed Old method new added better method with xpath
    -Removed old method to find foobar
    -new method uses xpath

--- foobar.py
+++ foobar.py
@@ -1,91 +1,36 @@
-import time
-import os
-import random
-import winsound
-from random import randint
-from threading import Thread
-from selenium import webdriver
-from PIL import Image
-#from twilio.rest import Client
 from selenium.webdriver.chrome.options import Options
 from ProgrammingTerms import *
 from ImageProcessor import ImageProcessor
-
-#Twilio Cilent setup
-#client = Client("AC***********************", "**********************") #This is for twilio account
+from selenium.webdriver.common.by import By
 
 #Chrome Driver Setup
 _chrome_options = Options()
-#_chrome_options.add_argument('user-data-dir=C:/Users/kiren/AppData/Local/Google/Chrome/User Data')
-#_chrome_options.add_argument('disable-infobars')
-chromedriver = "C:/Users/kiren/Desktop/Foobar-Hunter-Bot-master/chromedriver.exe"
+chromedriver = "chromedriver.exe"
 os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chromedriver,chrome_options=_chrome_options)
 
-def AddToFalsePositiveList(phrase):
-    with open('FPlist.txt','a') as file:
-        file.write(phrase + ",")  
-        file.close()
 
-def isInFalsePostitiveList(phrase):
-    if os.path.isfile('FPlist.txt'): 
-        with open('FPlist.txt','r') as file:
-            FalsePList = file.read()
-            file.close()
-        FalsePList = FalsePList.split(",")
-        if phrase in FalsePList:
-            return True
-    return False
 
-##def NotifyFoundMatch(): #This is for twilio account
-##    client.messages.create(to="+1303-******", 
-##                       from_="+720*******", 
-##                       body="Found It!")
-def Search():
-    iterations = 1 
+def check_page_for_foobar():
+    try:
+        driver.find_element(By.XPATH, '//div[contains(@data-url,"foobar")]')
+        input("Found it")
+    except:
+        pass
+
+def Search(): 
+    iterations = 1
+    driver = webdriver.Chrome(chromedriver,chrome_options=_chrome_options)
+    driver.delete_all_cookies()
     while True:
-        #phrase = GeneratePhrase(randint(1,2))
+        #phrase = "python command line arguments"
         phrase = get_from_winning_list()
-        if isInFalsePostitiveList(phrase):
-            continue
+        #phrase = GeneratePhrase(randint(1,2))
         print('Iteration: ' + str(iterations) + " Search-Criteria: " + phrase)
         driver.get('https://www.google.com/search?q='+ phrase)
-        print("waiting for foo...")
-        time.sleep(12)
-        driver.save_screenshot("ScreenShot.png")
-        ip = None
-        ip = ImageProcessor("ScreenShot.png")
-        print("    Processing Image...")
-        ip.process()
-        if ip.isMatch():
-            winsound.Beep(1000,300000)#<---------This Needs Better solution
-            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            print("!! --             !!Match Found!! --               !!")
-            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ")
-            print("_____________________________________________________")
-            print("Press FPC to add to FALSE POSITIVE LIST and Continue")
-            print("Press FPQ to Close the app.")
-            Input = input()
-            if Input.upper() == "FPC":
-                  AddToFalsePositiveList(phrase)
-                  print('SEARCH CRITERIA: '+ phrase + 'Added to FPList.txt')
-                  continue
-                  iterations+=1
-            elif Input.upper() == "FPQ":
-                  AddToFalsePositiveList(phrase)
-                  print('SEARCH CRITERIA: '+ phrase + 'Added to FPList.txt')
-                  sys.exit()
-            else:
-                  break
-        else:
-            iterations+=1
-            sleepTime = randint(3,3)
-            print('    Next search in ' + str(sleepTime) + " seconds...")
-            time.sleep(sleepTime)
-#--------------------------------------------------------------------------                  
+        check_page_for_foobar()
+        sleepTime = randint(5,30)
+        print("Sleeping for {}".format(sleepTime))
+        time.sleep(SleepTime)
+        iterations+=1
 #------Find the FooBar-- solve the foobar            
 Search()
-                 
-
-
-
